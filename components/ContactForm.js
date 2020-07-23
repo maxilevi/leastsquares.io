de786eca8a1d57e8c034199077a63761c49da377
Zaphyk <zaphyk@live.com>
2020-07-23 02:23:45 -0300

    make required

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -11,12 +11,12 @@ export default class ContactForm extends React.Component
                 description="Whatever your ambition, we’d love to design and build your next big idea or lend a hand on an existing one."
                 element={
                     <div className="mt-12">
-                        <form action="/contactSuccess" method="POST" netlify-data="true" className="grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
+                        <form action="/contact-success" method="POST" netlify-data="true" className="grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
                             <div>
                                 <label htmlFor="first_name" className="block text-sm font-medium leading-5 text-gray-700">First
                                     name</label>
                                 <div className="mt-1 relative rounded-md shadow-sm">
-                                    <input id="first_name"
+                                    <input id="first_name" required="true"
                                            className="form-input py-3 px-4 block w-full transition ease-in-out duration-150"/>
                                 </div>
                             </div>
@@ -24,13 +24,13 @@ export default class ContactForm extends React.Component
                                 <label htmlFor="last_name" className="block text-sm font-medium leading-5 text-gray-700">Last
                                     name</label>
                                 <div className="mt-1 relative rounded-md shadow-sm">
-                                    <input id="last_name"
+                                    <input id="last_name" required="true"
                                            className="form-input py-3 px-4 block w-full transition ease-in-out duration-150"/>
                                 </div>
                             </div>
                             <div className="sm:col-span-2">
                                 <label htmlFor="company"
-                                       className="block text-sm font-medium leading-5 text-gray-700">Company</label>
+                                       className="block text-sm font-medium leading-5 text-gray-700">Company (Optional)</label>
                                 <div className="mt-1 relative rounded-md shadow-sm">
                                     <input id="company"
                                            className="form-input py-3 px-4 block w-full transition ease-in-out duration-150"/>
@@ -40,7 +40,7 @@ export default class ContactForm extends React.Component
                                 <label htmlFor="email"
                                        className="block text-sm font-medium leading-5 text-gray-700">Email</label>
                                 <div className="mt-1 relative rounded-md shadow-sm">
-                                    <input id="email" type="email"
+                                    <input id="email" type="email" required="true"
                                            className="form-input py-3 px-4 block w-full transition ease-in-out duration-150"/>
                                 </div>
                             </div>
@@ -48,13 +48,13 @@ export default class ContactForm extends React.Component
                                 <label htmlFor="message"
                                        className="block text-sm font-medium leading-5 text-gray-700">Message</label>
                                 <div className="mt-1 relative rounded-md shadow-sm">
-                                    <textarea id="message" rows="4"
+                                    <textarea id="message" required="true" rows="4"
                                               className="form-textarea py-3 px-4 block w-full transition ease-in-out duration-150"></textarea>
                                 </div>
                             </div>
                             <div className="sm:col-span-2">
                                 <span className="w-full inline-flex rounded-md shadow-sm">
-                                    <button type="button"
+                                    <button type="submit"
                                             className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
                                       Let's talk
                                     </button>
