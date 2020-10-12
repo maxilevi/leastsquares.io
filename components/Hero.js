c2ddeb6ed52592eb43a91512845e4cc749c50c9d
Maximiliano Levi <maxilevi77@gmail.com>
2020-10-11 21:39:56 -0300

    remove Link tags

--- components/Hero.js
+++ components/Hero.js
@@ -37,20 +37,15 @@ export default class Hero extends React.Component
                             </p>
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div className="rounded-md shadow">
-                                    <Link href="#portfolio">
-                                        <a
-                                           className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                                            See our work
-                                        </a>
-                                    </Link>
+                                    <a href="#portfolio"
+                                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                                        See our work
+                                    </a>
                                 </div>
                                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                                    <Link href="/contact">
-                                        <a
-                                           className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                                            Contact us
-                                        </a>
-                                    </Link>
+                                    <a href="/contact" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                                        Contact us
+                                    </a>
                                 </div>
                             </div>
                         </div>
--- components/Portfolio.js
+++ components/Portfolio.js
@@ -102,12 +102,10 @@ export class ViewEntirePortfolioButton extends React.Component
         return (
             <div className="mt-12">
                 <div className="sm:col-span-2 relative max-w-xl mx-auto">
-                    <Link href={this.props.url}>
-                        <a
-                            className="w-auto flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-                            {this.props.text}
-                        </a>
-                    </Link>
+                    <a href={this.props.url}
+                        className="w-auto flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                        {this.props.text}
+                    </a>
                 </div>
             </div>
         );
